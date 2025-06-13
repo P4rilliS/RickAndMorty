3cc7ca07e1ec4d91c57d913daed0b05cbe260a6c
Sergio <sergioparilli06@gmail.com>
2025-06-12 20:18:07 -0400

    a

--- SCRIPT/characters.js
+++ SCRIPT/characters.js
@@ -1,8 +1,8 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const usuarioAutenticado = localStorage.getItem("autenticado")
-//   if (usuarioAutenticado !== "true") {
-//     window.location.href = "../index.html"}
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const usuarioAutenticado = localStorage.getItem("autenticado")
+  if (usuarioAutenticado !== "true") {
+    window.location.href = "../index.html"}
+});
 
 salir = document.getElementById("salir")
 
